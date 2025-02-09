40b6a9fd518d6e70dd164873c3409bb9b414a2e8
dnest2009 <dnest2009@gmail.com>
2025-02-09 19:27:09 +0200

    generator_numbers_first_commit

---
+++ generator_numbers.py
@@ -0,0 +1,14 @@
+import re
+from typing import Callable
+
+def generator_numbers(text:str):                                      
+    numbers= re.findall(r'[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?' ,text) # використовуємо регулярний вираз для знаходження чисел у тексті
+    for number in numbers:  
+        yield number            #вертаємо число зі списку у форматі string
+        
+def sum_profit(text: str, func: Callable):
+    total_income = sum([float(i) for i in func(text)])     #створюємо список з чисел у форматі float. Використовуємо ф-ію sum для підсумовування чисел
+    print(f"Загальний дохід: {total_income}")
+
+text ="Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
